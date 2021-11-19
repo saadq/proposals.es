4cc89a3e04e36bbd932302b5ccd6e93177f71f6e
Saad Quadri <saadquad@gmail.com>
2021-11-19 04:59:29 -0500

    Add redirect for wasm subdomain

--- next.config.js
+++ next.config.js
@@ -4,5 +4,21 @@ module.exports = {
   experimental: {
     scrollRestoration: true,
     styledComponents: true
+  },
+  rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'wasm.proposals.es'
+            }
+          ],
+          destination: '/wasm/:path*'
+        }
+      ]
+    }
   }
 }
