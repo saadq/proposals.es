bcb9e119957f97f1de4a92d953f4727b9ed59c4d
Saad Quadri <saadquad@gmail.com>
2021-11-19 03:48:37 -0500

    Add placeholder page for /wasm

---
+++ src/pages/wasm/index.tsx
@@ -0,0 +1,9 @@
+import { PageContainer } from '../../components/common'
+
+export default function WasmPage() {
+  return (
+    <PageContainer width="85%" margin="0 auto">
+      <h1>WASM</h1>
+    </PageContainer>
+  )
+}
