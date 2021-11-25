ad43c8ea4f62fa2f15664fc4153b16d5737b3933
Saad Quadri <saadquad@gmail.com>
2021-11-25 14:52:36 -0500

    Revert "Setup monorepo"
    This reverts commit f001d8940477caa5aa3be79ea37e169eb67a4af0.

---
+++ next.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    scrollRestoration: true,
+    styledComponents: true
+  },
+  rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'wasm.proposals.es'
+            }
+          ],
+          destination: '/wasm/:path*'
+        },
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'www.wasm.proposals.es'
+            }
+          ],
+          destination: '/wasm/:path*'
+        }
+      ]
+    }
+  }
+}
